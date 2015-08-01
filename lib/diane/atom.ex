96b1a215eb05c452a41005e089ffb99ebef4df6e
Patrick Brown <patrick.arthur.brown@gmail.com>
2015-08-01 08:33:45 -0400

    fix specs & disable atom parser

--- lib/diane/atom.ex
+++ lib/diane/atom.ex
@@ -1,42 +1,5 @@
 defmodule Diane.Atom do
-  import SweetXml
-  use Timex
-
-  @spec parse(String) :: {:ok, map} | {:error, RuntimeError}
   def parse(source) do
-    try do
-      parsed = source
-      |> parse_header
-      {:ok, parsed}
-    rescue
-      e in RuntimeError -> {:error, e}
-    end
-  end
-
-  @spec parse_header(String) :: map
-  def parse_header(source) do
-    source
-    |> xmap(
-      title: ~x"//feed/title/text()",
-      link: ~x"//feed/link/@href",
-      id: ~x"//feed/id/text()",
-      updated: ~x"//feed/updated/text()",
-      entries: [
-        ~x"//feed/entry"l,
-        title: ~x"./title/text()",
-        link: ~x"./link/@href",
-        id: ~x"./id/text()",
-        updated: ~x"./updated/text()",
-        published: ~x"./published/text()",
-        summary: ~x"./summary/text()",
-        content: ~x"./content/text()",
-        authors: [
-          ~x"./author"l,
-          name: ~x"./name/text()",
-          uri: ~x"./uri/text()",
-          email: ~x"./email/text()"
-        ]
-      ]
-    )
+    raise "Not implemented yet"
   end
 end
--- test/diane/rss_test.exs
+++ test/diane/rss_test.exs
@@ -4,31 +4,81 @@ defmodule Diane.RSSTest do
 
   @rss_file "test/fixtures/rss.xml"
 
-  test "it can properly parse an atom file" do
-    {:ok, source} = File.read(@rss_file)
+  def parsed_source do
+    @rss_file |> File.read! |> parse
+  end
 
-    expect = %Diane.RSS{
-      channels: [
-        %Diane.RSS.Channel{
-          description: 'xkcd.com: A webcomic of romance and math humor.',
-          items: [
-            %Diane.RSS.Item{
-              link: 'http://xkcd.com/1530/',
-              title: 'Keyboard Mash',
-              description: "<img src=\"http://imgs.xkcd.com/comics/keyboard_mash.png\" title=\"WHY DON'T YOU COME HANG OUT INSIDE MY HOUSE. WE CAN COOK BREAD AND CHAT ABOUT OUR INTERNAL SKELETONS.\" alt=\"WHY DON'T YOU COME HANG OUT INSIDE MY HOUSE. WE CAN COOK BREAD AND CHAT ABOUT OUR INTERNAL SKELETONS.\" />"
-             },
-            %Diane.RSS.Item{
-              description: "<img src=\"http://imgs.xkcd.com/comics/vodka.png\" title=\"Or whatever's handy! I'm pretty much pure alcohol and water, so it doesn't really matter!\" alt=\"Or whatever's handy! I'm pretty much pure alcohol and water, so it doesn't really matter!\" />",
-              link: 'http://xkcd.com/1528/',
-              title: 'Vodka'
-            }
-          ],
-          title: 'xkcd.com',
-          link: 'http://xkcd.com/'
-        }
-      ]
-    }
+  test "it contains all channels in feed" do
+    assert parsed_source.channels |> length == 1
+  end
 
-    assert parse(source) == {:ok, expect}
+  test "it contains the channel title" do
+    channel = parsed_source.channels |> List.first
+    assert channel.title == 'xkcd.com'
+  end
+
+  test "it contains the channel description" do
+    channel = parsed_source.channels |> List.first
+    assert channel.description == 'xkcd.com: A webcomic of romance and math humor.'
+  end
+
+  test "it contains the channel language" do
+    channel = parsed_source.channels |> List.first
+    assert channel.language == 'en'
+  end
+
+  test "it contains the channel link" do
+    channel = parsed_source.channels |> List.first
+    assert channel.link == 'http://xkcd.com/'
+  end
+
+
+  test "it contains all items in channel" do
+    item_count = parsed_source.channels
+    |> List.first
+    |> Map.fetch!(:items)
+    |> length
+
+    assert item_count == 2
+  end
+
+  test "it can parse the item bodies" do
+    item = parsed_source.channels
+    |> List.first
+    |> Map.fetch!(:items)
+    |> List.first
+    assert item.guid == 'http://xkcd.com/1530/'
+  end
+
+  test "it contains the item link" do
+    item = parsed_source.channels
+    |> List.first
+    |> Map.fetch!(:items)
+    |> List.first
+    assert item.link == 'http://xkcd.com/1530/'
+  end
+
+  test "it contains the item pub_date" do
+    item = parsed_source.channels
+    |> List.first
+    |> Map.fetch!(:items)
+    |> List.first
+    assert item.pub_date == 'Wed, 27 May 2015 04:00:00 -0000'
+  end
+
+  test "it contains the item title" do
+    item = parsed_source.channels
+    |> List.first
+    |> Map.fetch!(:items)
+    |> List.first
+    assert item.title == 'Keyboard Mash'
+  end
+
+  test "it contains the item description" do
+    item = parsed_source.channels
+    |> List.first
+    |> Map.fetch!(:items)
+    |> List.first
+    assert item.description == "<img src=\"http://imgs.xkcd.com/comics/keyboard_mash.png\" title=\"WHY DON'T YOU COME HANG OUT INSIDE MY HOUSE. WE CAN COOK BREAD AND CHAT ABOUT OUR INTERNAL SKELETONS.\" alt=\"WHY DON'T YOU COME HANG OUT INSIDE MY HOUSE. WE CAN COOK BREAD AND CHAT ABOUT OUR INTERNAL SKELETONS.\" />"
   end
 end

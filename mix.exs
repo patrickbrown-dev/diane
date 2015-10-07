a2f6ba264a3665ec3d31079665dc5539028f5438
Patrick Brown <patrick.arthur.brown@gmail.com>
2015-10-06 20:49:20 -0700

    Update mix.exs for hex.pm

--- mix.exs
+++ mix.exs
@@ -10,23 +10,23 @@ defmodule Diane.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [{:sweet_xml, "~> 0.2.1"}]
+  end
+
+  defp description do
+    """
+    RSS/Atom parser for Elixir.
+    """
+  end
+
+  defp package do
+    [maintainers: ["Patrick Arthur Brown"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/ptrckbrwn/diane"}]
   end
 end
